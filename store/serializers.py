7080889bd92e9fdcddc07452779037e2d862e0f6
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-05-09 13:32:05 +0700

    Use generic api view

--- store/serializers.py
+++ store/serializers.py
@@ -11,13 +11,7 @@ class CollectionSerializer(serializers.ModelSerializer):
         model = Collection
         fields = ['id', 'title', 'products_count']
 
-    products_count = serializers.IntegerField()
-
-    # products_count = serializers.SerializerMethodField(
-    #     method_name='calculate_products_count')
-
-    # def calculate_products_count(self, collection: Collection):
-    #     return Product.objects.filter(collection=collection).count()
+    products_count = serializers.IntegerField(read_only=True)
 
 
 class ProductSerializer(serializers.ModelSerializer):
