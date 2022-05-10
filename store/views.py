cdb682ea0bd92d0f84d2c4b21c0a74b3300b9aaa
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-05-10 12:29:02 +0700

    Delete a cart api

--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from django.db.models.aggregates import Count
 
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.pagination import PageNumberPagination
@@ -52,6 +52,9 @@ class ReviewViewSet(ModelViewSet):
         return {'product_id': self.kwargs['product_pk']}
 
 
-class CartViewSet(CreateModelMixin, GenericViewSet, RetrieveModelMixin):
+class CartViewSet(CreateModelMixin,
+                  GenericViewSet,
+                  RetrieveModelMixin,
+                  DestroyModelMixin):
     queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
